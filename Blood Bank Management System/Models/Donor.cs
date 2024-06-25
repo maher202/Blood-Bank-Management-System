43316d85353bf5ae054920990f7735bcaa71c5f6
omar Mohammad <omar@gmail.com>
2024-06-26 00:05:57 +0300

    initialize

---
+++ Blood Bank Management System/Models/Donor.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Blood_Bank_Management_System.Models
+
+{
+    public class Donor
+    {
+        
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+
+        public string Mail { get; set; }
+        [Required]
+
+        public string Password { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+        
+        public enum Gender
+        {
+        Male,
+        Famele
+        }
+        public enum Bloodtype 
+        {
+        O_positive,
+        O_negative,
+        B_positive,
+        B_negative,
+        A_positive,
+        A_negative,
+        AB_positive,
+        AB_negative,
+        }
+
+        public enum Disease {
+            No_thing,
+            Blood_pressure_disorders,
+            Thyroid_disease,
+            Diabetes,
+            Cancer,
+            Heart_disorders,
+            Hepatitis
+        }
+
+        public string? Other_Diseases { get; set; }
+
+        public DateTime? Latest_Donation  { get; set; }
+        
+
+
+    }
+}
---
+++ Blood Bank Management System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blood_Bank_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
